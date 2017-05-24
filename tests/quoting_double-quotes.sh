8c9886603c717199efa0f0a948a8a3e84c77907d
Niols <niols@niols.fr>
2017-05-24 14:43:13 +0200

    more tests about quoting

---
+++ tests/quoting_double-quotes.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# See [2.2 Quoting] and [2.2.3 Double-Quotes]
+
+echo "| & ; <> () \$ \` \\ \" ' 	 
+* ? [ # ~ = %"
---
+++ tests/quoting_single-quotes.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# See [2.2 Quoting] and [2.2.2 Single-Quotes]
+
+echo '| & ; <> () $ ` \ " 	
+ * ? [ # ~ = %'
