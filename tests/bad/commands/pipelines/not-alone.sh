50587f03421486a283c9c8d791f143e62afa3f05
Niols <niols@niols.fr>
2017-05-26 08:39:47 +0200

    add tests about pipelines and and/or

---
+++ tests/bad/commands/pipelines/not-alone.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# We are not alone in this universe. But this 'not' is.
+
+!
---
+++ tests/good/commands/pipelines.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# Geronimo! To the one that will have to read the CST to check if it's
+# OK, I want you to know that this is not personal.
+
+! a || b && c & d | e ; f && g || h
