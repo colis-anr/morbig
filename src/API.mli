50c253d6d032ab98e99967c0683a6c181ae5c643
Yann Regis-Gianas <yrg@irif.fr>
2017-06-06 20:47:45 +0200

    CST: Cosmetics.

--- src/API.mli
+++ src/API.mli
@@ -6,4 +6,4 @@ exception SyntaxError of Position.t * string
 (** [parse_file filename] performs the syntactic analysis of
    [filename] and returns a concrete syntax tree if [filename] content
    is syntactically correct. Raise {SyntaxError msg} otherwise. *)
-val parse_file : string -> CST.complete_command list
+val parse_file : string -> CST.complete_command_list
