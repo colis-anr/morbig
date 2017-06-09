d2f9e50ee5faa0adfba8785d1b6538f9870b782e
Niols <niols@niols.fr>
2017-06-09 13:32:20 +0200

    add a function to filter positions in Yojson.safe.t

--- src/engine.ml
+++ src/engine.ml
@@ -506,9 +506,30 @@ let parse contents =
   in
   parse (complete_command lexbuf.Lexing.lex_curr_p)
 
+let rec json_filter_positions =
+  let open Yojson.Safe in
+  function
+  | `Assoc sjl ->
+     if List.for_all (fun (s, j) -> s = "value" || s = "position") sjl then
+       let (_, j) = List.find (fun (s, _) -> s = "value") sjl in
+       json_filter_positions j
+     else
+       `Assoc (List.map (fun (s, j) -> Format.printf "%s@." s; (s, json_filter_positions j)) sjl)
+  | `Bool b -> `Bool b
+  | `Float f -> `Float f
+  | `Int i -> `Int i
+  | `Intlit s -> `Intlit s
+  | `List jl -> `List (List.map json_filter_positions jl)
+  | `Null -> `Null
+  | `String s -> `String s
+  | `Tuple jl -> `Tuple (List.map json_filter_positions jl)
+  | `Variant (s, None) -> `Variant (s, None)
+  | `Variant (s, Some j) -> `Variant (s, Some (json_filter_positions j))
+  
 let save_as_json cout csts =
-  CST.complete_command_list_to_json csts |>
-  Yojson.Safe.pretty_to_channel cout
+  CST.complete_command_list_to_json csts
+  |> json_filter_positions
+  |> Yojson.Safe.pretty_to_channel cout
 
 let other_scripts_magic_strings =
   List.map Str.regexp [
