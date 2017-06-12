21a4cfa3b4144095a8dd2364ca0c5623cba547d0
Ralf Treinen <treinen@free.fr>
2017-06-12 20:17:54 +0200

    expected output of quoting_single_quote

--- tests/good/2.2-quoting/2.2.2-single_quotes/quoting_single-quotes.sh
+++ tests/good/2.2-quoting/2.2.2-single_quotes/quoting_single-quotes.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 # See [2.2 Quoting] and [2.2.2 Single-Quotes]
+# The last symbol on the line starting with echo is a <tab>.
+# There should be one argument to echo, with an embedded >newline>.
 
 echo '| & ; <> () $ ` \ " 	
  * ? [ # ~ = %'
