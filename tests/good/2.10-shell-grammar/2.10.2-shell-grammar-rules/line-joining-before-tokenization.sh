a708d2668c86ea994ad65d5c080f4a3db0f54ce6
Ralf Treinen <treinen@free.fr>
2017-06-13 08:25:17 +0200

    good tests: great expectations

---
+++ tests/good/2.10-shell-grammar/2.10.2-shell-grammar-rules/line-joining-before-tokenization.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# note of 2.10.2 item 1 : line joining done before tokenization
+
+t\
+r\
+u\
+e\
---
+++ tests/good/2.4-reserved_words/keywords-not-quoted.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# no character of a keyword may be quoted
+
+'if'
+i'f'
+i"f"
+i\f
+
