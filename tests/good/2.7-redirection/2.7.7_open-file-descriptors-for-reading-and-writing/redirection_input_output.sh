fb60806417498a19f07cad3f1b45d741e0e90979
Ralf Treinen <treinen@irif.fr>
2017-06-15 17:41:39 +0200

    test case redirection input and output

--- tests/good/2.7-redirection/2.7.7_open-file-descriptors-for-reading-and-writing/redirection_input_output.sh
+++ tests/good/2.7-redirection/2.7.7_open-file-descriptors-for-reading-and-writing/redirection_input_output.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 # See [2.7.6 Duplicating an Output File Descriptor]
 
+# open word for reading and writing 
 echo <>word
 
+# open word for reading and writing on file descriptor 2
 echo 2<>word
+
+# open word for reading and writing, 4 is argument to echo
 echo 4 <>word
+
+# open word for reading and writring from file descriptor 5
 echo 5<> word
+
+# open word for reading and writing, 6 is argument to echo
 echo 6 <> word
