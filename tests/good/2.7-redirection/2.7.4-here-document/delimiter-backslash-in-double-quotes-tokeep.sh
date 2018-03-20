aadf868795884c97edf1e97e54618a0532cf76ce
Ralf Treinen <treinen@irif.fr>
2018-03-20 18:08:23 +0100

    2 test cases with \ inside double quotes in a here-document delimiter

---
+++ tests/good/2.7-redirection/2.7.4-here-document/delimiter-backslash-in-double-quotes-tokeep.sh
@@ -0,0 +1,14 @@
+# 2.2.3 Double-Quotes:
+# The <backslash> shall retain its special meaning as an escape character
+# (see Escape Character (Backslash)) only when followed by one of the
+# following characters when considered special:
+# $   `   "   \   <newline>
+
+# Hence: unquoting on the introductory occurrence of the delimiter must
+# not remove the backslash, here.
+
+if true
+then cat <<"E\OF"
+hello
+E\OF
+fi
