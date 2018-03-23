a67130b6f7e0f5de7acaa5fe50b200a4616a3162
Ralf Treinen <treinen@irif.fr>
2018-03-23 13:59:46 +0100

    test case continuation line in here document

---
+++ tests/good/2.7-redirection/2.7.4-here-document/continuation-line-in-here-document.sh
@@ -0,0 +1,13 @@
+# 2.7.4: if the delimiter is not quoted then backslash behaves as in double
+# quotes, that is <backslash><newline> indicates a line contunation.
+if true
+then
+cat << EOF
+first line
+second line
+third line with continuation\
+EOF
+forth and last line
+EOF
+else echo "baeh"
+fi
