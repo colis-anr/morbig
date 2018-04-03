0c5be6a3c733292ded757688dadb19da412a6ff2
Ralf Treinen <treinen@irif.fr>
2018-04-03 12:03:52 +0200

    main: cosmetics

--- src/morbig.ml
+++ src/morbig.ml
@@ -13,15 +13,26 @@
 
 open API
 
-let save filename (cst : CST.complete_command list) =
-  let cout = open_out (Options.output_file_of_input_file filename) in
+let save input_filename (cst : CST.complete_command list) =
+  (** write the concrete syntax tree [cst] to the output file corresponding
+      to [input_filename]. The format and and the name of the output file
+      are determined by the program options. *)
+  let cout = open_out (Options.output_file_of_input_file input_filename) in
   Options.(begin match backend () with
-  | Bin -> output_value cout (filename, cst)
-  | Json -> JsonHelpers.save_as_json false cout cst
-  | SimpleJson -> JsonHelpers.save_as_json true cout cst
-  end);
+           | Bin -> output_value cout (input_filename, cst)
+           | Json -> JsonHelpers.save_as_json false cout cst
+           | SimpleJson -> JsonHelpers.save_as_json true cout cst
+           end);
   close_out cout
-
+            
+let save_error input_filename message =
+  (** write string [message] to the error file corresponding to
+      [input_filename]. *)
+  let eout = open_out (input_filename ^ ".morbigerror") in
+  output_string eout message;
+  output_string eout "\n";
+  close_out eout
+                
 let string_of_exn = function
   | Errors.ParseError pos ->
      Printf.sprintf "%s: Syntax error."
@@ -37,31 +48,27 @@ let string_of_exn = function
   | e -> raise e
 
 let main =
+  let parse_one_file input_filename =
+    if Options.skip_nosh () &&
+         (Scripts.is_elf input_filename ||
+            Scripts.is_other_script input_filename)
+    then begin
+        Printf.eprintf "Skipping: %s.\n" input_filename;
+      end
+    else
+      try
+        parse_file input_filename |> save input_filename
+      with e -> if Options.continue_after_error ()
+                then
+                  save_error input_filename (string_of_exn e)
+                else (
+                  output_string stderr (string_of_exn e ^ "\n");
+                  exit 1
+                )
+  in
   Options.analyze_command_line_arguments ();
   if List.length (Options.input_files ()) <= 0 then (
     Printf.eprintf "morbig: no input files.\n";
     exit 1
   );
-  List.iter (function input ->
-      if Options.skip_nosh () &&
-         (Scripts.is_elf input || Scripts.is_other_script input)
-      then begin
-        Printf.eprintf "Skipping: %s.\n" input;
-      end
-      else
-        try
-          parse_file input |> save input
-        with e -> if Options.continue_after_error ()
-                  then
-                      let eout = open_out (input ^ ".morbigerror")
-                      in begin
-                          output_string eout (string_of_exn e);
-                          output_string eout "\n";
-                          close_out eout
-                        end
-                  else (
-                    output_string stderr (string_of_exn e ^ "\n");
-                    exit 1
-                  )
-            )
-            (Options.input_files ())
+  List.iter parse_one_file (Options.input_files ())
