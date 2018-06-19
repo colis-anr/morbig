e5e41936813c03ef0142d03f7f22fba624db741f
Yann Regis-Gianas <yrg@irif.fr>
2018-06-19 17:41:07 +0200

    Isolate PrelexerState code.

--- src/prelexerState.ml
+++ src/prelexerState.ml
@@ -1,4 +1,5 @@
 open CST
+open ExtPervasives
 
 (**specification:
 
@@ -81,3 +82,158 @@ let components_of_atom_list atoms =
 let components b =
   components_of_atom_list b.buffer
 
+let push_quoting_mark k b =
+  { buffer = QuotingMark k :: b.buffer }
+
+let pop_quotation k b =
+  let rec aux squote quote = function
+    | [] ->
+       (squote, quote, [])
+    | QuotingMark k' :: buffer when k = k' ->
+       (squote, quote, buffer)
+    | (AssignmentMark | QuotingMark _) :: buffer ->
+       aux squote quote buffer (* FIXME: Check twice. *)
+    | WordComponent (w, WordEmpty) :: buffer ->
+       aux (w ^ squote) quote buffer
+    | WordComponent (w, c) :: buffer ->
+       aux (w ^ squote) (c :: quote) buffer
+  in
+  (* The last character is removed from the quote since it is the
+     closing character. *)
+  let buffer = pop_character b.buffer in
+  let squote, quote, buffer = aux "" [] buffer in
+  let word = Word (squote, quote) in
+  let quoted_word =
+    match k with
+    | SingleQuote -> WordSingleQuoted word
+    | DoubleQuote -> WordDoubleQuoted word
+  in
+  let quote = WordComponent ("\"" ^ squote ^ "\"", quoted_word) in
+  { buffer = quote :: buffer }
+
+let push_assignment_mark current =
+  { buffer = AssignmentMark :: current.buffer }
+
+let recognize_assignment current =
+  let is_assignment_mark = function AssignmentMark -> true | _ -> false in
+  let rhs, prefix = take_until is_assignment_mark current.buffer in
+  if prefix = current.buffer then (
+    current
+  ) else
+    let current' = { buffer = rhs @ List.tl prefix } in
+    match prefix with
+    | AssignmentMark :: WordComponent (s, _) :: prefix ->
+       assert (s.[String.length s - 1] = '='); (* By after_equal unique call. *)
+       (* [s] is a valid name. We have an assignment here. *)
+       let lhs = String.(sub s 0 (length s - 1)) in
+       if Name.is_name lhs then (
+         let rhs_string = contents_of_atom_list rhs in
+         { buffer =
+             WordComponent (s ^ rhs_string,
+                            WordAssignmentWord (Name lhs, Word (rhs_string,
+                                                                components_of_atom_list rhs)))
+             :: prefix
+         }
+       ) else
+         (*
+            If [lhs] is not a name, then the corresponding word
+            literal must be merged with the preceding one, if it exists.
+          *) (
+         begin match List.rev rhs with
+         | WordComponent (s_rhs, WordLiteral s_rhs') :: rev_rhs ->
+            let word = WordComponent (s ^ s_rhs, WordLiteral (s ^ s_rhs')) in
+            { buffer = List.rev rev_rhs @ word :: prefix }
+         | _ ->
+            current'
+         end)
+    | _ -> current'
+
+(** [(return ?with_newline lexbuf current tokens)] returns a list of
+    pretokens consisting of, in that order:
+
+    - WORD(w), where w is the contents of the buffer [current] in case the
+      buffer [current] is non-empty;
+
+    - all the elements of [tokens];
+
+    - NEWLINE, in case ?with_newline is true (default: false).
+
+    We know that [tokens] does not contain any Word pretokens. In fact, the
+    prelexer produces Word pretokens only from contents he has collected in
+    the buffer.
+
+          *)
+let return ?(with_newline=false) lexbuf (current : prelexer_state) tokens =
+  assert (
+      not (List.exists (function (Pretoken.PreWord _)->true |_-> false) tokens)
+    );
+
+  let current = recognize_assignment current in
+
+  let flush_word b =
+    (* FIXME: Optimise! *)
+    let rec aux accu = function
+      | WordComponent (s, _) :: b -> aux (s ^ accu) b
+      | AssignmentMark :: b -> aux accu b
+      | QuotingMark _ :: _ -> assert false
+      | [] -> accu
+    in
+    aux "" b.buffer
+  and produce token =
+    (* FIXME: Positions are not updated properly. *)
+    (token, lexbuf.Lexing.lex_start_p, lexbuf.Lexing.lex_curr_p)
+  in
+  let is_digit d =
+    Str.(string_match (regexp "^[0-9]+$") d 0)
+  in
+  let followed_by_redirection = Parser.(function
+    | Pretoken.Operator (LESSAND |  GREATAND | DGREAT | CLOBBER |
+                         LESS | GREAT | LESSGREAT) :: _ ->
+      true
+    | _ ->
+      false
+  ) in
+
+  (*specification
+
+    2.10.1 Shell Grammar Lexical Conventions
+
+    The input language to the shell must be first recognized at the
+    character level. The resulting tokens shall be classified by
+    their immediate context according to the following rules (applied
+    in order). These rules shall be used to determine what a "token"
+    is that is subject to parsing at the token level. The rules for
+    token recognition in Token Recognition shall apply.
+
+    If the token is an operator, the token identifier for that
+    operator shall result.
+
+    If the string consists solely of digits and the delimiter character is
+    one of '<' or '>', the token identifier IO_NUMBER shall be
+    returned.
+
+    Otherwise, the token identifier TOKEN results.
+
+  *)
+
+  let buffered =
+    match flush_word current with
+    | "" ->
+      []
+    | w when is_digit w && followed_by_redirection tokens ->
+      [Pretoken.IoNumber w]
+    | w ->
+      let csts =
+        List.(flatten (rev_map (function
+            | WordComponent (_, WordEmpty) -> []
+            | WordComponent (_, s) -> [s]
+            | AssignmentMark -> []
+            | QuotingMark _ -> assert false
+         ) current.buffer))
+      in
+      [Pretoken.PreWord (w, csts)]
+  in
+  let tokens = if with_newline then tokens @ [Pretoken.NEWLINE] else tokens in
+  let tokens = buffered @ tokens in
+  let out = List.map produce tokens in
+  out
