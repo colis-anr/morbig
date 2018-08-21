435d70877cd02eee59c0681f5966ce3b950aab84
Yann Regis-Gianas <yrg@irif.fr>
2018-08-21 21:51:18 +0200

    C API.

---
+++ include/morbig.h
@@ -0,0 +1,121 @@
+/*
+
+  Copyright (C) 2017,2018 Yann Régis-Gianas, Nicolas Jeannerod,
+  Ralf Treinen.
+
+  This is free software: you can redistribute it and/or modify it
+  under the terms of the GNU General Public License, version 3.
+
+  Additional terms apply, due to the reproduction of portions of
+  the POSIX standard. Please refer to the file COPYING for details.
+
+*/
+
+#include <caml/mlvalues.h>
+
+/*
+ * Initialize morbig. This function must be called before any other
+ * calls to other functions of this API.
+ */
+void initialize_morbig (char** argv);
+
+/*
+ * The type for concrete syntax trees.
+ */
+typedef value cst_t;
+
+/*
+ * Parse `filename` and return the corresponding syntax tree if
+ * `filename` contains a syntactically valid shell script. If
+ * an error is detected, `NULL` is returned and `get_error ()`
+ * returns a human readable error message.
+ */
+cst_t parse_file (char* filename);
+
+/*
+ * If a call to `parse_file (filename)` produced an error,
+ * `get_error_message` returns a human readable error message
+ *
+ */
+char* get_error_message ();
+
+/*
+ * Let OCaml GC reclaim the concrete syntax tree.
+ */
+void  dispose_cst (cst_t cst);
+
+/*
+ * A concrete syntax tree is a tree with three kind of
+ * nodes:
+ *
+ * - LOCATION nodes annotate a tree with two positions that characterize
+ *   the portion of the source file that produced this tree.
+ *
+ * - NODE nodes correspond to the application of a POSIX grammar rule.
+ *   This application is specified by a rule name and a possibly empty
+ *   list of producers (i.e. concrete syntax subtrees).
+ *
+ * - DATA nodes correspond to literals.
+ *
+ */
+typedef enum kind {
+  LOCATION,
+  NODE,
+  DATA
+} kind_t;
+
+/* Return the kind of the node of the cst root. */
+kind_t get_kind (cst_t cst);
+
+/* The type for position in a source file. */
+typedef value position_t;
+
+/* Return the position filename. */
+char* get_filename_of_position (position_t p);
+
+/* Return the line number of the position. */
+int get_lnum_of_position (position_t p);
+
+/* Return the column number of the position. */
+int get_bol_of_position (position_t p);
+
+/* Return the offset of the position. */
+int get_cnum_of_position (position_t p);
+
+/* Return the starting position of the concrete syntax tree.
+ * Precondition: `get_kind(cst) == LOCATION`.
+ */
+position_t get_start_position (value cst);
+
+/* Return the ending position of the concrete syntax tree.
+ * Precondition: `get_kind(cst) == LOCATION`.
+ */
+position_t get_end_position (value cst);
+
+/* Return the located concrete syntax tree.
+ * Precondition: `get_kind(cst) == LOCATION`.
+ */
+cst_t get_located_value (value cst);
+
+/* Return the rule name.
+ * Precondition: `get_kind(cst) == NODE`.
+ */
+char* get_rule_name (value cst);
+
+/* Return the number of concrete subtrees.
+ * Precondition: `get_kind(cst) == NODE`.
+ */
+int get_number_of_children (value cst);
+
+/* Return the number of concrete subtrees.
+ * Precondition:
+ * `get_kind(cst) == NODE && k < get_number_of_children(cst)`.
+ */
+cst_t get_children (value cst, int k);
+
+/* Return the literal string representation.
+ * Precondition: `get_kind(cst) == DATA`
+ */
+char* get_data (value cst);
+
+
--- src/API.ml
+++ src/API.ml
@@ -14,3 +14,79 @@
 exception SyntaxError of CST.position * string
 
 let parse_file = Scripts.parse_file
+
+(* Called from C stub. *)
+
+type ccst =
+  | Location of CST.lexing_position * CST.lexing_position * ccst
+  | Node     of string * ccst array
+  | Data     of string
+
+let ccst_of_json_program j =
+  let rec aux = function
+    | `Assoc [ "value", v; "position", p ] ->
+       let start_p, end_p = location p in
+       Location (start_p, end_p, aux v)
+    | `List (`String k :: children) ->
+       Node (k, aux' (`List children))
+    | `Variant (k, None) ->
+       Node (k, [||])
+    | `Variant (k, Some children) ->
+       Node (k, aux' children)
+    | `String s ->
+       Data s
+    | `List l ->
+       Node ("Tuple", aux' (`List l))
+    | json ->
+       Printf.eprintf "Unexpected json: %s\n" (Yojson.Safe.pretty_to_string json);
+       assert false
+  and aux' = function
+    | `List c ->
+       Array.of_list (List.map aux c)
+    | `Assoc m ->
+       aux' (`List (snd (List.split m)))
+    | json ->
+       Printf.eprintf "Unexpected json: %s\n" (Yojson.Safe.pretty_to_string json);
+       assert false
+  and position = function
+    | `Assoc [ "pos_fname", `String pos_fname;
+               "pos_lnum", `Int pos_lnum;
+               "pos_bol", `Int pos_bol;
+               "pos_cnum", `Int pos_cnum ] ->
+       CST.({ pos_fname; pos_lnum; pos_bol; pos_cnum })
+    | json ->
+       Printf.eprintf "Unexpected json: %s\n" (Yojson.Safe.pretty_to_string json);
+       assert false
+
+  and location = function
+    | `Assoc [ "start_p", start_p; "end_p", end_p ] ->
+       (position start_p, position end_p)
+    | json ->
+       Printf.eprintf "Unexpected json: %s\n" (Yojson.Safe.pretty_to_string json);
+       assert false
+  in
+  aux j
+
+let ccst_roots = ref []
+
+let register cst =
+  ccst_roots := cst :: !ccst_roots;
+  cst
+
+exception CSTDisposalFailed
+
+let dispose_cst cst =
+  let rec aux = function
+    | [] ->
+       raise CSTDisposalFailed
+    | cst' :: ccsts ->
+       if cst == cst' then ccsts else cst' :: aux ccsts
+  in
+  aux !ccst_roots
+
+let untyped_parse_file s =
+  parse_file s |> CSTHelpers.program_to_json |> ccst_of_json_program |> register
+
+let _ =
+  Callback.register "untyped_parse_file" untyped_parse_file;
+  Callback.register "dispose_cst" dispose_cst
