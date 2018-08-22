ae35d2d6e7db834d86353e47b3bb826dc202140d
Niols <niols@niols.fr>
2018-08-22 17:44:51 +0200

    use apt when available to speed up CI; fix #39

--- .travis.install.sh
+++ .travis.install.sh
@@ -5,7 +5,7 @@ fold_start () { printf 'travis_fold:start:%s\r\033[33;1m%s\033[0m\n' "$1" "$2"; 
 fold_end () { printf 'travis_fold:end:%s\r' "$1"; }
 
 ## Install OPAM
-fold_start install_opam 'Install OPAM...'
+fold_start install_opam 'Install OPAM'
 mkdir -p ~/.local/bin
 wget https://raw.github.com/ocaml/opam/master/shell/opam_installer.sh -O - \
     | sh -s ~/.local/bin "$OCAML_VERSION"
@@ -14,6 +14,6 @@ eval $(opam config env)
 fold_end install_opam
 
 ## OPAM dependencies
-fold_start opam_deps 'Install OPAM dependencies...'
+fold_start opam_deps 'Install OPAM dependencies'
 opam install -y menhir yojson ppx_deriving_yojson visitors
 fold_end opam_deps
