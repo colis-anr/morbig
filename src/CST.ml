60ea38b278aefe913f38ab39e4286d9e5aef1f03
ishaangandhi <ishaangandhi@gmail.com>
2020-10-02 16:23:28 -0400

    Paramter length bug fix

--- src/CST.ml
+++ src/CST.ml
@@ -395,7 +395,7 @@ and variable =
 
 and variable_attribute =
   | NoAttribute
-  | ParameterLength of word
+  | ParameterLength 
   | UseDefaultValues of string * word
   | AssignDefaultValues of string * word
   | IndicateErrorifNullorUnset of string * word
