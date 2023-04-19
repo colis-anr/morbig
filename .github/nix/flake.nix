5409d9da70fa205e4e6cd2a901a13f5a0b2a0ba4
Nicolas Jeannerod <niols@niols.fr>
2023-04-19 14:10:10 +0100

    Nix environment and Nix-based CI (#155)

---
+++ .github/nix/flake.nix
@@ -0,0 +1,49 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    opam-nix.url = "github:tweag/opam-nix";
+    opam-nix.inputs.nixpkgs.follows = "nixpkgs";
+
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+    pre-commit-hooks.inputs.nixpkgs.follows = "nixpkgs";
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
+
+      imports = [
+        ./with-nixpkgs.nix
+        ./with-opam-nix.nix
+        inputs.pre-commit-hooks.flakeModule
+      ];
+
+      perSystem = { self', pkgs, config, ... }: {
+        formatter = pkgs.nixfmt;
+
+        packages.default = self'.packages.with-nixpkgs;
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs.ocamlPackages; [ ocaml-lsp ocp-indent ];
+          inputsFrom = [ self'.packages.default ];
+          shellHook = config.pre-commit.installationScript;
+        };
+
+        pre-commit.settings.hooks = {
+          dune-opam-sync.enable = true;
+          opam-lint.enable = true;
+          ocp-indent.enable = true;
+          nixfmt.enable = true;
+          deadnix.enable = true;
+        };
+      };
+
+      ## Improve the way `inputs'` are computed by also handling the case of
+      ## flakes having a `lib.${system}` attribute (eg. `opam-nix`).
+      perInput = system: flake:
+        if flake ? lib.${system} then { lib = flake.lib.${system}; } else { };
+    };
+}
---
+++ .github/nix/with-nixpkgs.nix
@@ -0,0 +1,36 @@
+{ ... }: {
+  perSystem = { pkgs, ... }:
+    let opkgs = pkgs.ocamlPackages;
+    in {
+      packages.with-nixpkgs = pkgs.stdenv.mkDerivation {
+        name = "morbig";
+        ## NOTE: The use of `../..` matters because the path is taken as relative to
+        ## the current file, and therefore to `.github/nix/`.
+        src = ../..;
+
+        nativeBuildInputs = with opkgs; [
+          ## Basic ones, always necessary
+          ocaml
+          dune_3
+          findlib
+          ## Specific to our project
+          menhir
+        ];
+
+        buildInputs = with opkgs; [
+          menhirLib
+          ppx_deriving_yojson
+          visitors
+          yojson
+        ];
+
+        buildPhase = ''
+          make build
+        '';
+
+        installPhase = ''
+          make install PREFIX=$out
+        '';
+      };
+    };
+}
