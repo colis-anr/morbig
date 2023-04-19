b8a1d63c31f2a4a7644eb00413ccdce02be83cab
Niols <niols@niols.fr>
2023-04-19 06:44:44 +0100

    Add other Nix environment using on `opam-nix`

--- .github/nix/flake.nix
+++ .github/nix/flake.nix
@@ -1,12 +1,22 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    opam-nix.url = "github:tweag/opam-nix";
+    opam-nix.inputs.nixpkgs.follows = "nixpkgs";
+
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
   outputs = inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" ];
-      imports = [ ./with-nixpkgs.nix ];
+
+      imports = [ ./with-nixpkgs.nix ./with-opam-nix.nix ];
+
+      ## Improve the way `inputs'` are computed by also handling the case of
+      ## flakes having a `lib.${system}` attribute (eg. `opam-nix`).
+      perInput = system: flake:
+        if flake ? lib.${system} then { lib = flake.lib.${system}; } else { };
     };
 }
---
+++ .github/nix/with-opam-nix.nix
@@ -0,0 +1,11 @@
+{ ... }: {
+  perSystem = { inputs', pkgs, ... }:
+    ## NOTE: The use of `../..` matters because the path is taken as relative to
+    ## the current file, and therefore to `.github/nix/`.
+    let
+      scope =
+        inputs'.opam-nix.lib.buildOpamProject { inherit pkgs; } "morbig" ../.. {
+          ocaml-base-compiler = "*";
+        };
+    in { packages.with-opam-nix = scope.morbig // { inherit scope; }; };
+}
