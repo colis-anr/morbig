cda079a8eea84f8363b33c704c9ad9f560d33572
Niols <niols@niols.fr>
2023-05-09 00:36:29 +0000

    Add two more cases for non-assignment words

--- tests/good/2.6-word-expansions/2.6.1-tilde-prefix/login.sh
+++ tests/good/2.6-word-expansions/2.6.1-tilde-prefix/login.sh
@@ -1,4 +1,6 @@
 echo ~knuth/foo/bar
+echo ~kn:/bonjour:~/bonsoir:and/this/one
+echo /bonjour:~kn:~/bonsoir:and/this/one
 X=~kn:~/bin:~darkvador/secret
 Y=~kn:/bonjour:~/bonsoir:and/this/one
 Z=/bonjour:~kn:~/bonsoir:and/this/one
