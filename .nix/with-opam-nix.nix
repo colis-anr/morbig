dcac78b23d61c2acd7578d067f41502a3276566d
Niols <niols@niols.fr>
2023-05-10 13:14:49 +0000

    Resolve doc and test dependencies with `opam-nix`

--- .nix/with-opam-nix.nix
+++ .nix/with-opam-nix.nix
@@ -3,9 +3,10 @@
     ## NOTE: The use of `./..` matters because the path is taken as relative to
     ## the current file, and therefore to `.nix/`.
     let
-      scope =
-        inputs'.opam-nix.lib.buildOpamProject { inherit pkgs; } "morbig" ./.. {
-          ocaml-base-compiler = "*";
-        };
+      scope = inputs'.opam-nix.lib.buildOpamProject {
+        inherit pkgs;
+        resolveArgs.with-doc = true;
+        resolveArgs.with-test = true;
+      } "morbig" ./.. { ocaml-base-compiler = "*"; };
     in { packages.with-opam-nix = scope.morbig // { inherit scope; }; };
 }
