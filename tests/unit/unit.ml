e516749fe2f1dfc20df0bee73a24954578341618
Niols <niols@niols.fr>
2023-05-23 21:19:32 +0000

    Add simple unit tests

---
+++ tests/unit/unit.ml
@@ -0,0 +1,17 @@
+(**************************************************************************)
+(*  Copyright (C) 2017-2023 Yann Régis-Gianas, Nicolas Jeannerod,         *)
+(*  Ralf Treinen.                                                         *)
+(*                                                                        *)
+(*  This is free software: you can redistribute it and/or modify it       *)
+(*  under the terms of the GNU General Public License, version 3.         *)
+(*                                                                        *)
+(*  Additional terms apply, due to the reproduction of portions of        *)
+(*  the POSIX standard. Please refer to the file COPYING for details.     *)
+(**************************************************************************)
+
+let () =
+  Alcotest.run
+    "unit"
+    [
+      ("name", Name.test_cases);
+    ]
